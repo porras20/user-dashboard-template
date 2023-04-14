5ff37ee29061e79b6a2c6949fbc32ad55ea559eb
Juan Jose Porras <juanjoseporras2019@gmail.com>
2023-04-14 10:04:43 -0500

    Se creo sidebar y top cards

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='flex justify-between px-4 pt-4'>
+        <h2>Dashboard</h2>
+        <h2>Bienvenido de nuevo</h2>
+    </div>
+  )
+}
